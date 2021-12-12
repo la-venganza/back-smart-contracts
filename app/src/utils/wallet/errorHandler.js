eed29e5d16a314402c3db487e133938c16bd7d19
joacolopez <joacolopez@balloon-group.com>
2021-12-12 00:30:26 -0300

    remove unused code

--- app/src/utils/wallet/errorHandler.js
+++ app/src/utils/wallet/errorHandler.js
@@ -1,19 +1,7 @@
 const idInUse = require('../../errors/idInUse')
 
 function handleError(error){
-    if (error instanceof idInUse) {
-        console.log("id in use ")
-        message = error.message + ' | ' + error.detail.msg
-        throw new ServerError(error, message, error.status || 500)
-    } else if (error.request) {
-        //custom error for unresponsive server
-        console.log("no response from python service")
-        throw new ConnectionError(error, 'Python Service is not responding')
-    } else {
-        // Something happened in setting up the request and triggered an Error
-        console.log('Error', error.message);
-        throw new Error(error)
-    }
+    throw new error(error);
 }
 
 module.exports = handleError
